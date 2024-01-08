260a4619a2aa810288da42545391dbf9404696f7
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-01-08 16:31:33 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+function runGame() {}
 
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivisionQuestion() {}
