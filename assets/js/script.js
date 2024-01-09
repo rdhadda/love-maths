58ba19ebe1c17302c6209ddbaa4be48a88043cf6
Raj.Dhadda <Raj.Dhadda@UK-L-TRESC14385>
2024-01-09 11:39:49 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("You clicked Submit!");
       } else {
         let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+  runGame("addition");
 });
 
 /**
@@ -21,9 +22,16 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed.
  */
 
-function runGame() {
+function runGame(gameType) {
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
 function checkAnswer() {}
@@ -34,7 +42,11 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
+}
 
 function displaySubtractQuestion() {}
 
